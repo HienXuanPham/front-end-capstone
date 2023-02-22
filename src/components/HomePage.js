4c781912fd8b19a5b43d3091813bb986574a2702
Xuan Hien Pham <phamxuanhien02@gmail.com>
2023-02-21 19:12:54 -0500

    removed import Container in HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import "../styles/HomePage.css";
 
